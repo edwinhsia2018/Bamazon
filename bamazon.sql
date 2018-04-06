1c43b21beb541ba300af443749bc0faf3348477d
Edwin Hsia <edwinhsia@gmail.com>
2018-04-06 10:49:45 -0500

    Friday Updates

--- bamazon.sql
+++ bamazon.sql
@@ -12,3 +12,18 @@ CREATE TABLE products (
     stock_quantity INTEGER (10),
     primary key (item_id)
 );
+
+INSERT INTO products 
+(product_name, department_name, price, stock_quantity)
+VALUE
+("Pampers Diapers", "Baby", 21.99, 20),
+("Macbook Pro 15", "Computers", 2799.99, 10),
+("iPad Pro 10.5", "Computers", 929.99, 15),
+("Apple Magic Mouse", "Computer Accessories", 59.99, 40),
+("Munchkin Milk Bottle", "Baby", 8.99, 50),
+("Organic, Non-GMO, BPA-Free, Vitamin-Infused Baby Wipes", "Baby", 79.99, 100),
+("Wilson Evolution Basketball", "Sports", 59.99, 37),
+("Grand Slam Baseball Bat", "Sports", 15.99, 25),
+("Nalgene Water Bottle", "Outdoor", 16.99, 87),
+("Camping Stove", "Outdoor", 39.99, 13),
+("Bear with it Beary-Flavored Bear Spray", "Outdoor", 9.99, 140)
